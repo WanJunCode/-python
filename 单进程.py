7d19b29bf301d9608a06677602ec16d12ef574d8
wanjun <wanjuncode@163.com>
2018-05-25 12:28:40 +0800

    Add files via upload

---
+++ 单进程.py
@@ -0,0 +1,17 @@
+from multiprocessing import Process
+import time
+
+
+def task(msg):
+    print('hello, %s' % msg)
+    time.sleep(1)
+
+
+if __name__ == '__main__':
+
+    p = Process(target=task, args=('world',))
+    p.start()
+
+    if p.is_alive():
+        print('Process: %s is running' % p.pid)
+    p.join()
